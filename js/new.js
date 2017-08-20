13c91f45394f6c3bc04ac81ed1e2c6e5b7d6221b
grispire <grispire@gmail.com>
2017-08-20 18:34:21 +0300

    save it now

--- js/new.js
+++ js/new.js
@@ -1,18 +1,42 @@
 "use strict"
 document.addEventListener("DOMContentLoaded", function() {
   var settings = {
-    gameMode: 0,
-    gameField: 0,
-    gameTime: 0,
+    //получение режима игры из input[type="range"]
+    gameTime: document.querySelector('#game-time__input').value,
+    //получение режима игры из input[type="radio"]
+    gameMode: document.querySelector('input[name="game-mode__input"]:checked').value,
+    //получение режима игры из input[type="radio"]
+    fieldSize: document.querySelector('input[name="game-field__input"]:checked').value,
+
   }
-  console.log(document.querySelectorAll('input[name="game-mode__input"]'));
-  var test1 = document.querySelectorAll('input[name="game-mode__input"]')
-  var test = document.getElementsByName("game-mode__input");
-  console.log(typeof test1)
-  console.log(typeof test);
-  for (var i = 0; i < test.length; i++) {
-    if (test[i].checked) {
-      console.log(test[i].value);
+  //слежка за изменением настроек в блоке настроек
+  document.querySelector("#game-settings").addEventListener("change", setSettings);
+
+  //функция, изменяющая объект settings, отвечающий за настройки
+  //сам игровой процесс изменится после запуска новой игры
+  function setSettings(event) {
+    //получено изменение размера поля, будет изменен размера поля
+    if (event.target.name == 'game-field__input') setFieldSize(event.target.value);
+    //получено изменение режима игры, будет изменен режим игры
+    if (event.target.name == 'game-mode__input') setGameMode(event.target.value);
+    //получено изменение времени, будет изменено время
+    if (event.target.name == 'game-time__input') setGameTime(event.target.value);
+
+    //функция установки игрового времени
+    function setGameTime(value) {
+      settings.gameTime = value;
     }
+
+    //функция установки режима игры
+    function setGameMode(value) {
+      settings.gameMode = value;
+    }
+
+    //функция установки размера поля
+    function setFieldSize(value) {
+      settings.fieldSize = value;
+    }
+    console.log('Настройки изменены. Игровое время: ' + settings.gameTime + '; Игровой режим: ' + settings.gameMode + '; Размер поля: ' + settings.fieldSize);
   }
+
 });
