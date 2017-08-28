c524491c7f6690ac1ba14dd69cb74e9e2be2c8ca
grispire <grispire@gmail.com>
2017-08-28 15:50:24 +0300

    new UI, hotkeys, information

--- js/new.js
+++ js/new.js
@@ -8,6 +8,8 @@ var
   settings = new Settings(),
   //статистика
   stats = new Stats(),
+  //поп-апы
+  popups = new Popups(),
   //игровые звуки
   sounds = {
     tick: document.querySelector('#sound-tick'),
@@ -18,6 +20,7 @@ var
     playing: false,
     prevPepe: -1,
   },
+  timers = {},
   btnPlay = document.querySelector("#play");
 //конструктор объекта настроек
 function Settings() {
@@ -55,9 +58,8 @@ function Stats() {
     timeLeft = 0,
     score = 0,
     highscore = (localStorage.getItem('highscore')) ? +localStorage.getItem('highscore') : 0,
-    domTimeLeft = document.querySelector("#game-timeLeft h4"),
-    domScore = document.querySelector("#game-score h4"),
-    domHighscore = document.querySelector("#game-highscore h4")
+    docScore = document.querySelector("#game-score h4"),
+    popupScore = document.querySelector("#gameover-score");
   this.setTimeLeft = function(timeNumber) {
     timeLeft = +timeNumber;
     this.showTimeLeft();
@@ -66,24 +68,26 @@ function Stats() {
     return timeLeft;
   }
   this.showTimeLeft = function() {
-    domTimeLeft.textContent = "Time left: " + timeLeft;
+    document.querySelector("#game-timeLeft h4").textContent = "Time left: " + timeLeft;
   }
   this.setScore = function(scoreNumber) {
     score = scoreNumber;
-    domScore.textContent = "Score: " + scoreNumber;
+    docScore.textContent = "Score: " + scoreNumber;
   }
   this.addScore = function(scoreToAdd) {
     score += scoreToAdd;
-    domScore.textContent = "Score: " + score;
+    docScore.textContent = "Score: " + score;
   }
   this.getScore = function() {
     return score;
   }
   this.showScore = function() {
-    domScore.textContent = "Score: " + score;
+    docScore.textContent = "Score: " + score;
+    popupScore.textContent = score;
   }
   this.setHighscore = function(highscoreNumber) {
     if (highscoreNumber > highscore) {
+      document.querySelector("#gameover-highscore").textContent = 'New highscore - ' + highscoreNumber + '! (Instead ' + highscore + ')'
       highscore = +highscoreNumber;
       localStorage.setItem('highscore', highscoreNumber)
     }
@@ -92,9 +96,54 @@ function Stats() {
     return highscore;
   }
   this.showHighscore = function() {
-    domHighscore.textContent = "Highscore: " + highscore;
+    document.querySelector("#game-highscore h4").textContent = "Highscore: " + highscore;
   }
 };
+
+//конструктор объекта поп-апов
+function Popups() {
+  //закрывает все поп-апы и их обертку
+  this.close = function(e) {
+    //если в функцию передан эвент
+    if (e) {
+      //и у него есть класс popup-close, то был нажат крестик в окне поп-апа
+      if (e.target.classList.contains('popup-close')) {
+        //в таком случае закрываются все поп-апы
+        this.close();
+      }
+      //если эвент передан не был, то функция вызвана из кода
+    } else {
+      //все открытые всплывающие окна записываются в переменную
+      var popups = document.querySelectorAll(".popup-visible");
+      //и каждое из них
+      popups.forEach(function(item) {
+        //получает CSS свойства, прячущие слой
+        item.style.visibility = 'hidden';
+        item.classList.remove('popup-visible');
+      });
+    }
+  }
+  //показывает поп-ап по выбранному селектору
+  this.show = function(selector) {
+    //сначала закрывает все остальные всплывающие окна
+    this.close();
+    //переменная получает в себя элемент, выбранный по селектору
+    var popup = document.querySelector(selector);
+    //если переменная ошибочка
+    if (!popup) {
+      console.log('Неверный id всплывающего окна!');
+      return;
+    }
+    //отображает обязательную обложку всплывающего окна
+    document.querySelector("#popup-wrapper").style.visibility = 'visible';
+    document.querySelector("#popup-wrapper").classList.add('popup-visible');
+    //и отображает выбранный поп-ап
+    popup.style.visibility = 'visible';
+    popup.classList.add('popup-visible');
+    //добавляет поп-апу листенер на клик, который вызывает проверку на необходимость закрытия окна
+    popup.addEventListener('click', this.close.bind(this));
+  }
+}
 
 function setSettings() {
   settings.setFieldSize();
@@ -111,6 +160,29 @@ function showStats() {
 //ПЕРВИЧНЫЙ ВЫВОД ПОЛЯ И СТАТИСТИКИ
 drawField()
 showStats();
+
+//главный листенер клавиатуры
+document.addEventListener('keydown', function(e) {
+  //вызывает функцию только тогда, когда идет игра
+  if (ingameInfo.playing === true) {
+    keydownOnPepe(+e.key);
+  }
+  //нажатие R начинает новую игру
+  if (e.code == 'KeyR') {
+    if (ingameInfo.playing) gameOver(false);
+    play();
+  }
+  //нажатие P прерывает текущую игру, если она запущена
+  if (e.ctrlKey && e.code == 'KeyX') {
+    popups.close();
+  }
+  if (e.code == 'KeyP' && ingameInfo.playing) {
+    //вызов функции окончания игры без показа всплывающего окна и учета highscore
+    gameOver(false, false);
+    stats.setTimeLeft(0);
+    headerButtonsSwitcher('stop');
+  }
+});
 
 //функция создания поля
 function drawField() {
@@ -196,10 +268,6 @@ gameField.addEventListener('mousedown', function(e) {
     clickOnPepe(gameBlock);
   }
 });
-//также слушает клавиатуру
-document.addEventListener('keydown', function(e) {
-  keydownOnPepe(+e.key);
-});
 
 //обработка клика по активной клетке
 function clickOnPepe(gameBlock) {
@@ -209,6 +277,27 @@ function clickOnPepe(gameBlock) {
     scorer();
     //отображается новый Пепе
     showPepe();
+  }
+};
+
+//фукнция получает в себя начало('start') или конец('stop') игры, в зависимости от этого меняет "объем" кнопок в header
+function headerButtonsSwitcher(gameStatus) {
+  if (gameStatus === 'start') {
+    //кнопка "PLAY" теряет объем
+    document.querySelector('#play').classList.remove('button-clickable');
+    //зато его получают кнопки "REPLAY" & "BREAK"
+    document.querySelector('#replay').classList.add('button-clickable');
+    document.querySelector("#break").classList.add('button-clickable');
+  }
+  else if (gameStatus === 'stop') {
+    //кнопка "PLAY" получает объем
+    document.querySelector('#play').classList.add('button-clickable');
+    //зато его теряют кнопки "REPLAY" & "BREAK"
+    document.querySelector('#replay').classList.remove('button-clickable');
+    document.querySelector("#break").classList.remove('button-clickable');
+  }
+  else {
+    console.log('Неверный параметр передан в функцию');
   }
 }
 //нампад для взаимодействия с Пепе
@@ -229,25 +318,34 @@ function keydownOnPepe(key) {
   }
 }
 
+//сбрасывает все setInterval's в объекте timers, хранящем таймера
+function clearTimers() {
+  for (var timer in timers) {
+    clearInterval(timers[timer]);
+  };
+}
+
 function play() {
-  //если игра не идет в данный момент
-  if (!ingameInfo.playing) {
-    setSettings();
-    //передает в статистику время на игру
-    stats.setTimeLeft(settings.getGameTime());
-    //обнуляет очки
-    stats.setScore(0);
-    //показать статистику
-    showStats();
-    //нарисовать игровое поле
-    drawField();
-    //добавить первую активную клетку
-    showPepe();
-    ingameInfo.playing = true;
-    //листенер клавиатуры
-  }
+  //если игра идет в данный момент, то прерывается выполнение функции
+  if (ingameInfo.playing) return
+  //предварительно закрываются все поп-апы
+  popups.close();
+  //обновляется переменная настроек из инпутов
+  setSettings();
+  //передает в статистику время на игру
+  stats.setTimeLeft(settings.getGameTime());
+  //обнуляет очки
+  stats.setScore(0);
+  //показать статистику
+  showStats();
+  //нарисовать игровое поле
+  drawField();
+  //добавить первую активную клетку
+  showPepe();
+  ingameInfo.playing = true;
+  headerButtonsSwitcher('start');
   //игровой таймер
-  var gameSessionTimer = setInterval(function() {
+  timers.clasicTimer = setInterval(function() {
     //переменная получает из статистики оставшееся игровое время, изначально полученное из настроек
     var timer = stats.getTimeLeft();
     //переменная уменьшается с каждой итерацией интервала
@@ -261,27 +359,70 @@ function play() {
     //если время вышло
     if (timer === 0 || timer < 0) {
       //очищается таймер
-      clearInterval(gameSessionTimer);
+      clearTimers();
       //заканчивается игра
       gameOver();
       //в конце игры звучит гудок
       sounds.timeEnd.play();
     }
   }, 1000)
-
-  //функция окончания игры
-  function gameOver() {
-    //удаляется листенер с клика
-    document.querySelector('.pepe-standard').removeEventListener('mousedown', clickOnPepe);
-    //удаляется активная клетка
-    document.querySelector('.pepe-standard').classList.remove('game-block__active', 'pepe-standard');
-    //переменная, отвечающая за состояние игры
-    ingameInfo.playing = false;
-    //устанавливается highscore
-    stats.setHighscore(stats.getScore());
-    //обновляется вся статистика
-    showStats();
-  }
+}
+//функция окончания игры, принимает:
+// showPopup = true/false для показа всплывающего окна, по умолчанию true
+// setHighscore = true/false для учета очков в highscore, по умолчанию true
+function gameOver(showPopup = true, setHighscore = true) {
+  clearTimers();
+  //удаляется листенер с клика
+  document.querySelector('.pepe-standard').removeEventListener('mousedown', clickOnPepe);
+  //удаляется активная клетка
+  document.querySelector('.pepe-standard').classList.remove('game-block__active', 'pepe-standard');
+  //переменная, отвечающая за состояние игры
+  ingameInfo.playing = false;
+  //устанавливается highscore
+  if(setHighscore) stats.setHighscore(stats.getScore());
+  //обновляется вся статистика
+  showStats();
+  //если в функцию явно не передан запрет на показ всплывающего окна - оно показывается
+  if(showPopup) popups.show("#popup-gameover");
+  headerButtonsSwitcher('stop');
 }
 //запуск игры
 btnPlay.addEventListener('click', play);
+
+//листенер на клик по информационным кнопкам для вызова всплывающих окон
+document.querySelector("#info-buttons").addEventListener('click', function(e) {
+  switch (e.target.id) {
+    case 'controls-info':
+      popups.show("#popup-controls");
+      break;
+    case 'help-info':
+      popups.show("#popup-help");
+      break;
+    case 'version-info':
+      popups.show("#popup-version");
+      break;
+    case 'contacts-info':
+      popups.show('#popup-contacts');
+      break;
+    default:
+      return;
+  }
+});
+
+//листенер для кнопки BREAK, прерывает игру без зачисления highscore и вызова popup'а
+document.querySelector("#break").addEventListener('click', function() {
+  //если игра не запущена - выход из функции, т.к прерывать нечего
+  if (!ingameInfo.playing) return
+  //вызов функции окончания игры без показа всплывающего окна и учета highscore
+  gameOver(false, false);
+  stats.setTimeLeft(0);
+  headerButtonsSwitcher('stop');
+});
+
+//листенер для кнопки REPLAY, начинает новую игру в том случае, если игра уже идет
+document.querySelector("#replay").addEventListener('click', function() {
+  //если игра уже запущена - нажимать PLAY, выход из функции
+  if (!ingameInfo.playing) return
+  gameOver(false);
+  play();
+});
